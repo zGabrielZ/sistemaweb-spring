33f0911876f6b02a986893ddc1a4cbabe3fc6e36
= <=>
2020-05-17 01:57:33 -0300

    Sistema MVC

--- src/main/java/com/gabrielferreira/projeto/service/exceptions/DatabaseException.java
+++ src/main/java/com/gabrielferreira/projeto/service/exceptions/DatabaseException.java
@@ -6,7 +6,7 @@ public class DatabaseException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	public DatabaseException(String msg) {
-		super(msg);
+		super("Não pode excluir, pois tem entidades relacionadas");
 	}
 	
 
