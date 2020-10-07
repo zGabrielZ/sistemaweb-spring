98c23f1942baf012505107941dab7fb49d46a6b4
= <ferreiragabriel2612@gmail.com>
2020-10-07 17:35:47 -0300

    Criando a entidade Curso se relacionando da classe Aluno

--- src/main/java/com/gabrielferreira/projeto/modelo/entidade/Aluno.java
+++ src/main/java/com/gabrielferreira/projeto/modelo/entidade/Aluno.java
@@ -1,6 +1,8 @@
 package com.gabrielferreira.projeto.modelo.entidade;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
@@ -21,6 +23,10 @@ public class Aluno extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
 	private String ra;
+	
+	@ManyToOne
+	@JoinColumn(name = "curso_id")
+	private Curso curso;
 	
 	public Aluno(Long id, String nomeCompleto, String cpf, Sexo sexo, String ra) {
 		super(id, nomeCompleto, cpf, sexo);
