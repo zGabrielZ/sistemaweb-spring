51ec55068b2cf1fbc509e5e64aa71093eae6a9ae
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2022-01-12 17:26:06 -0300

    Index Controller criado

---
+++ src/main/java/com/gabrielferreira/projeto/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira.projeto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/index")
+	public String paginaInicial() {
+		return "index";
+	}
+}
